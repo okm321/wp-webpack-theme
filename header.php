9a341f40513630e5f8b0e12afa7cd5e1578566e0
okm321 <naofumi.okamoto@casting-one.jp>
2023-09-23 15:39:32 +0900

    header, footer

--- header.php
+++ header.php
@@ -1,59 +1,29 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package wp-webpack-theme
- */
+<!DOCTYPE html>
+<html>
 
-?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+  <meta charset="UTF-8">
+  <meta name="keywords" content="ヴェルディ, 米子, 鳥取, 新築分譲マンション, 中古マンション, 一戸建て, 信和不動産">
+  <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1,maximum-scale=1" />
 
-	<?php wp_head(); ?>
+  <!-- ファビコンアイコン -->
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/inc/images/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180"
+    href="<?php echo get_template_directory_uri(); ?>/inc/images/icon180.png">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/inc/images/favicon.ico">
+  <!--スマホ検索時表示部分指定-->
+  <PageMap>
+    <DataObject type="thumbnail">
+      <Attribute name="src" value="<?php echo get_template_directory_uri(); ?>/inc/images/icon180.png" />
+      <Attribute name="width" value="100" />
+      <Attribute name="height" value="130" />
+    </DataObject>
+  </PageMap>
+  <meta name="thumbnail" content="<?php echo get_template_directory_uri(); ?>/inc/images/favicon.ico" />
+
+  <link href="<?php echo get_template_directory_uri(); ?>/dist/css/style.css" type="text/css" rel="stylesheet">
+  <?php wp_head(); ?>
+
 </head>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wp-webpack-theme' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$wp_webpack_theme_description = get_bloginfo( 'description', 'display' );
-			if ( $wp_webpack_theme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $wp_webpack_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wp-webpack-theme' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+<body>
